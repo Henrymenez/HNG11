570387cf5e6a060caf5075a14473aa1088ad4a25
Henrymenez <henryugochukwuagu@gmail.com>
2024-07-01 01:06:11 +0100

    Add project files.

---
+++ StageOne/Controllers/VisitorInfoController.cs
@@ -0,0 +1,57 @@
+﻿using IPinfo;
+using IPinfo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+namespace StageOne.Controllers
+{
+
+    [ApiController]
+    [Route("api/hello")]
+    public class VisitorInfoController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+        private readonly IPinfoClient _client;
+
+        public VisitorInfoController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            string token = "e364ac0f4bf2d3";
+            _client = new IPinfoClient.Builder()
+                .AccessToken(token)
+                .Build();
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetVisitorInfo([FromQuery][Required] string visitor_name)
+        {
+
+            // Get the client's IP address
+            string ip = HttpContext.Connection.RemoteIpAddress?.ToString() ?? "127.0.0.1";
+
+            /*// Get the location information based on IP
+            var locationInfo = await _httpClient.GetAsync($"https://ipapi.co/{clientIp}/city/");*/
+            // making API call
+            //string ip = "216.239.36.21";
+            IPResponse ipResponse = await _client.IPApi.GetDetailsAsync(ip);
+            if (ipResponse != null)
+            {
+                
+                var response = new
+                {
+                    client_ip = ipResponse.IP,
+                    location = ipResponse.City,
+                    greeting = $"Hello, {visitor_name}!, the temperature is 11 degrees Celsius in {ipResponse.City}"
+                };
+
+                return Ok(response);
+
+            }
+
+            return BadRequest("Unable to get location information.");
+        }
+    }
+
+
+}
+
---
+++ StageOne/Domain/Dtos/LocationInfo.cs
@@ -0,0 +1,13 @@
+﻿namespace StageOne.Domain.Dtos
+{
+    public class LocationInfo
+    {
+        public string Ip { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Region { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public string Postal { get; set; } = string.Empty;
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
